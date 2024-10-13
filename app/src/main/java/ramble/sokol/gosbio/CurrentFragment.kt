34c988f272dc6a53a4493b5fafadac361ce085ce
RambleTOT <rambletot@gmail.com>
2024-10-13 13:26:52 +0500

    add service 2

--- app/src/main/java/ramble/sokol/gosbio/CurrentFragment.kt
+++ app/src/main/java/ramble/sokol/gosbio/CurrentFragment.kt
@@ -5,19 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ramble.sokol.gosbio.databinding.FragmentCurrentBinding
+import ramble.sokol.gosbio.databinding.FragmentStorisBinding
 
 class CurrentFragment : Fragment() {
 
+    private var binding: FragmentCurrentBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current, container, false)
+        binding = FragmentCurrentBinding.inflate(inflater, container, false)
+        val view = binding!!.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding!!.buttonLogin.setOnClickListener{
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            val verifFragment = VerifFragment()
+            transaction.replace(R.id.layout_fragment, verifFragment)
+            transaction.disallowAddToBackStack()
+            transaction.commit()
+        }
     }
 }
